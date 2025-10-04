d240c44cdc68fd095a21c2b89332ac30b18f66e8
Alex Hofmann <alexhoffy99@gmail.com>
2025-10-04 23:22:17 +0000

    the memory card seems to be getting the sentence from the day before. we

--- src/lib/firebase-service.ts
+++ src/lib/firebase-service.ts
@@ -33,9 +33,9 @@ export type Memory = {
 
 export const getMemoryDocId = (date: Date): string => {
   const dateObj = new Date(date);
-  const day = String(dateObj.getUTCDate()).padStart(2, '0');
-  const month = String(dateObj.getUTCMonth() + 1).padStart(2, '0');
-  const year = dateObj.getUTCFullYear();
+  const day = String(dateObj.getDate()).padStart(2, '0');
+  const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+  const year = dateObj.getFullYear();
   return `${year}-${month}-${day}`;
 };
 
